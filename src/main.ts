77fb84e6c8f482eddba58530e31ec80daac69210
Carolyn Hop3 <cmhope@ucsc.edu>
2024-10-15 22:54:07 -0700

    Button fix attempt

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,16 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Grey Goo";
 document.title = gameName;
 
-
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-let matter : number = 0;
+let matter: number = 0;
 
 const gatherButton = document.createElement("button");
-gatherButton.innerHTML = 'Gather Materials';
-gatherButton.onclick = function()
-{
-    matter++;
-    console.log(matter);
-}
+gatherButton.innerHTML = "Gather Materials";
+gatherButton.onclick = function () {
+  matter++;
+  console.log(matter);
+};
+app.append(gatherButton);
