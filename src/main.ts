96637dcaa6f0b24d69a797386509b240d69016f7
Carolyn Hop3 <cmhope@ucsc.edu>
2024-10-16 00:33:56 -0700

    fixed unused variable

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,8 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-const UPS = 20; //Updates per second
-let TS : number = 0; //Timestamp
+//const UPS = 20; //Updates per second
+let TS: number = 0; //Timestamp
 
 let matter: number = 0;
 let hNodes: number = 0;
@@ -26,13 +26,15 @@ gatherButton.onclick = function () {
 };
 app.append(gatherButton);
 
-
 const statusText = document.createElement("div");
 statusText.innerHTML =
-  "You are a self-replicating nanite.\nYou have " + (Math.floor(matter * 10))/10 + " units of machine-relevant matter.\nYou have " + (hNodes) + " other nodes in your swarm.";
+  "You are a self-replicating nanite.\nYou have " +
+  Math.floor(matter * 10) / 10 +
+  " units of machine-relevant matter.\nYou have " +
+  hNodes +
+  " other nodes in your swarm.";
 app.append(statusText);
 //setInterval(updateMat, 1000 / UPS);
-
 
 const harvesterButton = document.createElement("button");
 harvesterButton.innerHTML = "🤖Create Harvester Node⚒️: 10";
@@ -46,10 +48,15 @@ app.append(harvesterButton);
 requestAnimationFrame(updateMat);
 
 function updateMat() {
-  const nTS = performance.now(); //new timestamp  
-  matter += (hNodes * 1) * ((nTS-TS)/1000);
+  const nTS = performance.now(); //new timestamp
+  matter += hNodes * 1 * ((nTS - TS) / 1000);
   harvesterButton.disabled = matter < 10;
-  statusText.innerHTML = "You are a self-replicating nanite.\nYou have " + (Math.floor(matter * 10))/10 + " units of machine-relevant matter.\nYou have " + (hNodes) + " other nodes in your swarm.";
-  TS = nTS;  
+  statusText.innerHTML =
+    "You are a self-replicating nanite.\nYou have " +
+    Math.floor(matter * 10) / 10 +
+    " units of machine-relevant matter.\nYou have " +
+    hNodes +
+    " other nodes in your swarm.";
+  TS = nTS;
   requestAnimationFrame(updateMat);
 }
