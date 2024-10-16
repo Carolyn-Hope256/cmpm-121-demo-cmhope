ad5348bc82b5ef5bc4c922dfe2bb9185fcfa1eb8
Carolyn Hop3 <cmhope@ucsc.edu>
2024-10-15 23:17:08 -0700

    Completed steps 2 and 3.

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,15 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+const UPS = 1; //Updates per second
+
 let matter: number = 0;
+//let hNodes: number = 0;
+//let rNodes: number = 0;
+//let lNodes: number = 0;
+//let bNodes: number = 0;
+
+
 
 const gatherButton = document.createElement("button");
 gatherButton.innerHTML = "Gather Materials";
@@ -18,3 +26,15 @@ gatherButton.onclick = function () {
   console.log(matter);
 };
 app.append(gatherButton);
+
+let statusText = document.createElement("div");
+statusText.innerHTML = "You have " + matter + " units of machine-relevant matter.";
+app.append(statusText);
+setInterval(updateMat, 1000/UPS);
+
+
+
+function updateMat(){
+    matter += 1/UPS;
+    statusText.innerHTML = "You have " + matter + " units of machine-relevant matter.";
+}
