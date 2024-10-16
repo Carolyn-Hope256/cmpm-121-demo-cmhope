2266d7fe1db3f7534fbf275c14846b74c2adedee
Carolyn Hop3 <cmhope@ucsc.edu>
2024-10-15 22:51:27 -0700

    Hopefully added a button

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,17 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Grey Goo";
 document.title = gameName;
 
+
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+let matter : number = 0;
+
+const gatherButton = document.createElement("button");
+gatherButton.innerHTML = 'Gather Materials';
+gatherButton.onclick = function()
+{
+    matter++;
+    console.log(matter);
+}
