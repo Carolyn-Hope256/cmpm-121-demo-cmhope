29df208b7cb710ef00ba48ae3fbe5091e7af5ce8
Carolyn Hop3 <cmhope@ucsc.edu>
2024-10-15 22:31:36 -0700

    Changed game name

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My amazing game";
+const gameName = "Grey Goo";
 document.title = gameName;
 
 const header = document.createElement("h1");
