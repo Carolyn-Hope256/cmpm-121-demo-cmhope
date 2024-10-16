c55b2444d3f4fe360c5c3a78cc10c6b3320da027
Carolyn Hop3 <cmhope@ucsc.edu>
2024-10-15 23:51:00 -0700

    Completed steps 4 and 5, added emojis to buttons

--- src/main.ts
+++ src/main.ts
@@ -9,32 +9,47 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-const UPS = 1; //Updates per second
+const UPS = 20; //Updates per second
+let TS : number = 0; //Timestamp
 
 let matter: number = 0;
-//let hNodes: number = 0;
+let hNodes: number = 0;
 //let rNodes: number = 0;
 //let lNodes: number = 0;
 //let bNodes: number = 0;
 
-
-
 const gatherButton = document.createElement("button");
-gatherButton.innerHTML = "Gather Materials";
+gatherButton.innerHTML = "⛏️Gather Materials⚙️";
 gatherButton.onclick = function () {
   matter++;
   console.log(matter);
 };
 app.append(gatherButton);
 
-let statusText = document.createElement("div");
-statusText.innerHTML = "You have " + matter + " units of machine-relevant matter.";
+
+const statusText = document.createElement("div");
+statusText.innerHTML =
+  "You are a self-replicating nanite.\nYou have " + (Math.floor(matter * 10))/10 + " units of machine-relevant matter.\nYou have " + (hNodes) + " other nodes in your swarm.";
 app.append(statusText);
-setInterval(updateMat, 1000/UPS);
+//setInterval(updateMat, 1000 / UPS);
 
 
+const harvesterButton = document.createElement("button");
+harvesterButton.innerHTML = "🤖Create Harvester Node⚒️: 10";
+harvesterButton.onclick = function () {
+  matter -= 10;
+  hNodes += 1;
+  console.log(matter);
+};
+app.append(harvesterButton);
 
-function updateMat(){
-    matter += 1/UPS;
-    statusText.innerHTML = "You have " + matter + " units of machine-relevant matter.";
+requestAnimationFrame(updateMat);
+
+function updateMat() {
+  const nTS = performance.now(); //new timestamp  
+  matter += (hNodes * 1) * ((nTS-TS)/1000);
+  harvesterButton.disabled = matter < 10;
+  statusText.innerHTML = "You are a self-replicating nanite.\nYou have " + (Math.floor(matter * 10))/10 + " units of machine-relevant matter.\nYou have " + (hNodes) + " other nodes in your swarm.";
+  TS = nTS;  
+  requestAnimationFrame(updateMat);
 }
