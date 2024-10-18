9e42bffd4e98f4f3f34950812b085a7baf1fe9b9
Carolyn Hop3 <cmhope@ucsc.edu>
2024-10-18 16:38:58 -0700

    completed steps 8 and 9. Finding the correct typing for buttons[] caused delays.

--- src/main.ts
+++ src/main.ts
@@ -11,17 +11,40 @@ app.append(header);
 
 //const UPS = 20; //Updates per second
 let TS: number = 0; //Timestamp
-
 let matter: number = 0;
-let hNodes: number = 0;
+
+interface Item {
+  name: string;
+  cost: number;
+  rate: number;
+  amount: number;
+}
+
+function buy(i: Item) {
+  const cost = i.cost;
+  i.cost *= 1.15;
+  i.amount += 1;
+  return cost;
+}
+
+const availableItems: Item[] = [
+  { name: "🤖Create Harvester Node⚒️", cost: 10, rate: 0.1, amount: 0 },
+  { name: "🤖Create Refiner Node⚗️", cost: 100, rate: 2, amount: 0 },
+  { name: "🤖Create Logistics Node🫀", cost: 1000, rate: 50, amount: 0 },
+];
+
+const buttons: HTMLButtonElement[] = [];
+
+/*let hNodes: number = 0;
 let hPrice: number = 10;
 let rNodes: number = 0;
 let rPrice: number = 100;
 let lNodes: number = 0;
-let lPrice: number = 1000;
+let lPrice: number = 1000;*/
 //let bNodes: number = 0;
 
 const gatherButton = document.createElement("button");
+console.log(typeof gatherButton);
 gatherButton.innerHTML = "⛏️Gather Materials⚙️";
 gatherButton.onclick = function () {
   matter++;
@@ -39,80 +62,59 @@ app.append(statusText1);
 const statusText2 = document.createElement("div");
 statusText2.innerHTML =
   "You have " +
-  (hNodes + rNodes + lNodes) +
+  0 +
   " other nodes in your swarm. " +
   " Your swarm is generating " +
-  (hNodes * 0.1 + rNodes * 2 + lNodes * 50) +
+  0 +
   " matter per second.";
 app.append(statusText2);
 //setInterval(updateMat, 1000 / UPS);
 
-const harvesterButton = document.createElement("button");
-harvesterButton.innerHTML = "🤖Create Harvester Node⚒️: " + round(hPrice);
-harvesterButton.onclick = function () {
-  matter -= hPrice;
-  hPrice *= 1.15;
-
-  hNodes += 1;
-  console.log(matter);
-};
-app.append(harvesterButton);
-
-const refinerButton = document.createElement("button");
-refinerButton.innerHTML = "🤖Create Refiner Node⚗️: " + round(rPrice);
-refinerButton.onclick = function () {
-  matter -= rPrice;
-  rPrice *= 1.15;
-
-  rNodes += 1;
-  console.log(matter);
-};
-app.append(refinerButton);
-
-const logisticsButton = document.createElement("button");
-logisticsButton.innerHTML = "🤖Create Logistics Node🫀: " + round(lPrice);
-logisticsButton.onclick = function () {
-  matter -= lPrice;
-  lPrice *= 1.15;
-  
-  lNodes += 1;
-  console.log(matter);
-};
-app.append(logisticsButton);
+for (let i = 0; i < availableItems.length; i++) {
+  buttons[i] = document.createElement("button");
+  buttons[i].innerHTML =
+    availableItems[i].name + ": " + round(availableItems[i].cost);
+  buttons[i].onclick = function () {
+    matter -= buy(availableItems[i]);
+  };
+  app.append(buttons[i]);
+}
 
 requestAnimationFrame(updateMat);
 
 function updateMat() {
   const nTS = performance.now(); //new timestamp
-  matter += (hNodes * 0.1 + rNodes * 2 + lNodes * 50) * ((nTS - TS) / 1000);
-
-  harvesterButton.disabled = matter < hPrice;
-  refinerButton.disabled = matter < rPrice;
-  logisticsButton.disabled = matter < lPrice;
+  let rate = 0;
+  let nodes = 0;
+  for (let i = 0; i < availableItems.length; i++) {
+    rate += availableItems[i].rate * availableItems[i].amount;
+    nodes += availableItems[i].amount;
+  }
+  matter += rate * ((nTS - TS) / 1000);
 
   statusText1.innerHTML =
     "You are a self-replicating nanite.\nYou have " +
-    round(matter)+
+    round(matter) +
     " units of machine-relevant matter.";
 
   statusText2.innerHTML =
     "You have " +
-    (hNodes + rNodes + lNodes) +
+    nodes +
     " other nodes in your swarm. " +
     " Your swarm is generating " +
-    round(hNodes * 0.1 + rNodes * 2 + lNodes * 50) +
+    round(rate) +
     " matter per second.";
 
-  harvesterButton.innerHTML = "🤖Create Harvester Node⚒️: " + round(hPrice);
-    
-  refinerButton.innerHTML = "🤖Create Refiner Node⚗️: " + round(rPrice);
-
-  logisticsButton.innerHTML = "🤖Create Logistics Node🫀: " + round(lPrice);
+  for (let i = 0; i < availableItems.length; i++) {
+    buttons[i].innerHTML =
+      availableItems[i].name + ": " + round(availableItems[i].cost);
+    buttons[i].disabled = matter < availableItems[i].cost;
+  }
 
   TS = nTS;
   requestAnimationFrame(updateMat);
 }
 
-function round(n : number){
-    return(Math.floor(n * 10) / 10);
+function round(n: number) {
+  return Math.floor(n * 10) / 10;
 }
